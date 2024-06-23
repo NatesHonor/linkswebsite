992b6649ebdc236f02a1d72bd1fc9d0d0bb7a6e4
NatesHonor <nathaniel.j.marcellus@gmail.com>
2024-06-23 10:11:15 -0400

    Finalized Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faTwitter, faInstagram, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseOver = () => setIsHovered(true);
+  const handleMouseOut = () => setIsHovered(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <div className="header">
+        <h1>My Links</h1>
+        <p>Connect with me through my social media channels and check out my portfolio.</p>
+      </div>
+      <div className="links">
+        <a href="https://www.linkedin.com/in/nmarcellus/" target="_blank" className="link linkedin" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
         </a>
-      </header>
+        <a href="https://twitter.com/yourprofile" target="_blank" className="link twitter" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} /> Twitter
+        </a>
+        <div 
+          className={`link instagram ${isHovered ? 'hovered' : ''}`} 
+          onMouseOver={handleMouseOver} 
+          onMouseOut={handleMouseOut}
+        >
+          <FontAwesomeIcon icon={faInstagram} /> {isHovered ? '9.5k Followers' : 'Instagram'}
+        </div>
+        <a href="https://github.com/NatesHonor/" target="_blank" className="link github" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} /> GitHub
+        </a>
+        <a href="https://natemarcellus.com/portfolio" target="_blank" className="link portfolio" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGoogle} /> Portfolio
+        </a>
+      </div>
     </div>
   );
 }
